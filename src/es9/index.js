cf9109e5449ca606919124116fd602ad969b4e80
Joaquin Grunwald <joaquingrunwald@gmail.com>
2022-05-30 21:30:35 -0300

    Añadida nueva folder y archivos js

--- src/es9/index.js
+++ src/es9/index.js
@@ -1,27 +1,67 @@
-//Clase 8 async await
+//Operador de reposo
+const obj = {
+  name: "Oscar",
+  age: 32,
+  country: "MX",
+};
+
+let { name, ...all } = obj;
+console.log(name, all);
+
+//Si quiero name y age solamente:
+const obj = {
+  name: "Oscar",
+  age: 32,
+  country: "MX",
+};
+
+let { country, ...all } = obj;
+console.log(all);
+
+//Propiedades de propagación:------------------------
+const obj = {
+  name: "Oscar",
+  age: 32,
+};
+
+const obj1 = {
+  ...obj,
+  country: "MX",
+};
+
+console.log(obj1);
+
+//.finally():------------------------------------------
 const helloWorld = () => {
   return new Promise((resolve, reject) => {
-    true
-      ? setTimeout(() => resolve("Hello World"), 3000)
-      : reject(new Error("Test Error aaahhhh"));
+    true ? resolve("Hello World") : reject(new Error("Test Error"));
   });
 };
 
-const helloAsync = async () => {
-  const hello = await helloWorld();
-  console.log(hello);
+helloWorld()
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Finalizó"));
+
+//Ahora con un setTimeout:
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("Hello World 2"), 2000)
+      : reject(new Error("Test Error"));
+  });
 };
 
-helloAsync();
+helloWorld()
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Finalizó"));
 
-//Con try catch
-const anotherFunction = async () => {
-  try {
-    const hello = await helloWorld();
-    console.log(hello);
-  } catch (error) {
-    console.error(error);
-  }
-};
+//Regex
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+const match = regexData.exec("2020-05-14");
+const year = match[1];
+const month = match[2];
+const day = match[3];
 
-anotherFunction();
+console.log(year, day);
